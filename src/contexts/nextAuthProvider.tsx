d3c89b629daa272a82b6c9c254892bdd5951f681
Carlos <uppaplicativo@gmail.com>
2024-05-06 23:33:23 -0300

    first commit

--- src/contexts/nextAuthProvider.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-import { ReactNode } from 'react'
-import { SessionProvider } from "next-auth/react";
-
-export default function ProviderNextAuth({children}:{children?:ReactNode}) {
-  return  <SessionProvider>{children}</SessionProvider>
-}
